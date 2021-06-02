ca7e563c3118ba64017a56b5ec14f91b0cb1b35c
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-06-02 17:12:34 +0530

    Ability to add new contacts in the addressBook

--- AddressBook.js
+++ AddressBook.js
@@ -45,7 +45,7 @@ class Contact {
         return this._city;
     }
     set city(city) {
-        const cityPattern = RegExp("^[A-Z]{1}[a-z]{3,}$");
+        const cityPattern = RegExp('^[A-Za-z ]{3,}$');
         if (cityPattern.test(city))
             this._city = city;
         else throw "Invalid City";
@@ -65,17 +65,17 @@ class Contact {
         return this._zip;
     }
     set zip(zip) {
-        const pinPattern = RegExp("^([1-9])(\\S){2}(\\s)?\\S{2}[0-9]$");
+        const pinPattern = RegExp("^[0-9]{6}$");
         if(pinPattern.test(zip))
             this._zip = zip;
-        else throw "Invalid Zip"
+        else throw "Invalid Zip: " + zip;
     }
 
     get phoneNumber() {
         return this._phoneNumber;
     }
     set phoneNumber(phoneNumber) {
-        const phonePattern = RegExp("^[1-9]{1}[0-9]{9}$");
+        const phonePattern = RegExp("^[0-9]{2}[ ]*[0-9]{10}$");
         if(phonePattern.test(phoneNumber))
             this._phoneNumber = phoneNumber;
         else throw "Invalid Phone Number";
@@ -85,7 +85,7 @@ class Contact {
         return this._email;
     }
     set email(email) {
-        const emailPattern = RegExp("^([a-z]){1,}[a-z0-9]*([.+_-]){0,1}[0-9a-z]+(@){1}([0-9a-z]+)(\\.([a-z]){2,}){1}(\\.[a-z]{2,})?$");
+        const emailPattern = RegExp("^[a-zA-Z0-9+_-]+([.][a-zA-Z0-9]+)*@([a-zA-Z0-9]+)([.][a-z]+)?[.][a-z]{2,}$");
         if(emailPattern.test(email))
             this._email = email;
         else throw "Invalid EmailID";
@@ -97,9 +97,16 @@ class Contact {
     }
 }
 
+let conatctBookArray = new Array();
+
 try {
-let contact = new Contact("Nishi", "Gautam", "Mayur", "Lucknow", "UP", "627361", "8732671298", "nishigautam@gmail.com");
-console.log(contact.toString());
+    contact = new Contact("Nishi", "Gautam", "Mayur", "Lucknow", "UP", 627361, "91 8732671298", "nishigautam@gmail.com");
+    conatctBookArray.push(contact);
+    conatctBookArray.push(new Contact("Joe", "Adam", "Vistar", "Delhi", "NCR", 602821, "91 9832178328", "Joe123@gmail.com"));
+    conatctBookArray.push(new Contact("Aman", "Asthana", "Banglore", "Karnatka", 226016, "91 8992174839", "Alex1290Gorden@gmail.com"));
+    conatctBookArray.forEach(contact => console.log(contact.toString()));
 } catch (e) {
     console.error(e);
 }
+        
+//console.log(conatctBookArray.toString());
